6797d7a1059c105b5674c204896e629275caf56a
kgtkr <kgtkr.jp@gmail.com>
2019-10-08 00:35:36 +0900

    Raw[I]のIを型パラメーターでなく関連型にした

--- src/main/scala/domain/models/Raw.scala
+++ src/main/scala/domain/models/Raw.scala
@@ -3,17 +3,23 @@ package net.kgtkr.twitter_tools.domain.models
 import java.time.OffsetDateTime
 import io.circe.Json
 
-sealed trait Raw[I] {
+sealed trait Raw {
+  type I;
+
   val id: I;
   val createdAt: OffsetDateTime;
   val raw: Json
 }
 
 final case class UserRaw(id: UserId, createdAt: OffsetDateTime, raw: Json)
-    extends Raw[UserId];
+    extends Raw {
+  type I = UserId
+}
 
 final case class StatusRaw(
     id: StatusId,
     createdAt: OffsetDateTime,
     raw: Json
-) extends Raw[StatusId];
+) extends Raw {
+  type I = StatusId
+}
