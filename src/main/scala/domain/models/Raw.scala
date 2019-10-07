70b4b5cda235c01fedff15868e40e3cceed8ac23
kgtkr <kgtkr.jp@gmail.com>
2019-10-07 20:31:16 +0900

    Rawモデル作成

---
+++ src/main/scala/domain/models/Raw.scala
@@ -0,0 +1,19 @@
+package net.kgtkr.twitter_tools.domain.models
+
+import java.time.OffsetDateTime
+import io.circe.Json
+
+sealed trait Raw {
+  val id: Long;
+  val createdAt: OffsetDateTime;
+  val raw: Json
+}
+
+final case class UserRaw(val id: Long, val createdAt: OffsetDateTime, raw: Json)
+    extends Raw;
+
+final case class StatusRaw(
+    val id: Long,
+    val createdAt: OffsetDateTime,
+    raw: Json
+) extends Raw;
