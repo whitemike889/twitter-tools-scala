593522528e2e60068cffc2412fd18dafa606ceab
kgtkr <kgtkr.jp@gmail.com>
2019-10-08 00:23:16 +0900

    AppErrorの型定義変更
    `extends AppError[Nothing, E]`、`extends AppError[U, Nothing]`を`extends AppError[U, E]`に変更

--- src/main/scala/domain/models/AppError.scala
+++ src/main/scala/domain/models/AppError.scala
@@ -2,6 +2,6 @@ package net.kgtkr.twitter_tools.domain.models;
 
 sealed trait AppError[+U, +E];
 object AppError {
-  final case class Unknown[+U](error: U) extends AppError[U, Nothing];
-  final case class Normal[+E](error: E) extends AppError[Nothing, E];
+  final case class Unknown[+U, +E](error: U) extends AppError[U, E];
+  final case class Normal[+U, +E](error: E) extends AppError[U, E];
 }
