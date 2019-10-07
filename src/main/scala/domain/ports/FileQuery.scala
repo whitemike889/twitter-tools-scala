d6485e847f36e95ca19f904f5326f2d928d98d2e
kgtkr <kgtkr.jp@gmail.com>
2019-10-07 14:31:25 +0900

    FileQueryのドキュメントコメント書いた

--- src/main/scala/domain/ports/FileQuery.scala
+++ src/main/scala/domain/ports/FileQuery.scala
@@ -11,8 +11,22 @@ object FileQueryError {
 trait FileQuery[F[_]] {
   type Result[A] = EitherT[F, AppError[Any, FileQueryError], A]
 
+  /**
+    * ファイルを読み込む
+    * ファイルが存在しない、または指定されたパスがディレクトリならNotFound
+    */
   def readFile(path: String): Result[String]
+
+  /**
+    * ファイルまたはディレクトリが存在するか確認する
+    */
   def exists(path: String): Result[Boolean]
+
+  /**
+    * 特定ディレクトリの子ディレクトリまたは子ファイルの名前一覧を返す
+    * ディレクトリが存在しない、または指定されたパスがファイルならNotFound
+    *
+    */
   def readDir(path: String): Result[List[String]]
 }
 
