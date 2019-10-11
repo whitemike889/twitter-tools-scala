cd453c46470523c8c52c88c0a89b1f1f37a094a5
kgtkr <kgtkr.jp@gmail.com>
2019-10-11 18:15:31 +0900

    TwitterClientQuerySYMの結果型をReaderT Tokenにして、UserCacheもそうした

--- src/main/scala/domain/ports/TwitterClientQuery.scala
+++ src/main/scala/domain/ports/TwitterClientQuery.scala
@@ -3,15 +3,19 @@ package net.kgtkr.twitter_tools.domain.ports;
 import net.kgtkr.twitter_tools.domain.models.UserId
 import net.kgtkr.twitter_tools.domain.models.Token
 import net.kgtkr.twitter_tools.domain.models.UserRaw
+import net.kgtkr.twitter_tools.domain.models.Token
+import cats.data.ReaderT
 
 trait TwitterClientQuerySYM[F[_]] {
-  def fetchAuthUserId(): F[UserId]
+  type Result[A] = ReaderT[F, Token, A]
 
-  def fetchFollowers(userId: UserId): F[List[UserId]]
+  def fetchAuthUserId(): Result[UserId]
 
-  def fetchFriends(userId: UserId): F[List[UserId]]
+  def fetchFollowers(userId: UserId): Result[List[UserId]]
 
-  def lookupUsers(ids: List[UserId]): F[List[UserRaw]]
+  def fetchFriends(userId: UserId): Result[List[UserId]]
+
+  def lookupUsers(ids: List[UserId]): Result[List[UserRaw]]
 }
 
 object TwitterClientQuerySYM {
--- src/main/scala/domain/services/UserCache.scala
+++ src/main/scala/domain/services/UserCache.scala
@@ -4,14 +4,20 @@ import cats.syntax.monad
 import net.kgtkr.twitter_tools.domain.models.UserId
 import cats.Monad
 import cats.implicits._
+import cats._
+import cats.data._
 import net.kgtkr.twitter_tools.domain.ports.RawRepositoryCmdSYM
 import net.kgtkr.twitter_tools.domain.ports.RawRepositoryQuerySYM
 import net.kgtkr.twitter_tools.domain.ports.TwitterClientQuerySYM
 import net.kgtkr.twitter_tools.domain.models.Raw
 import net.kgtkr.twitter_tools.domain.models.UserRaw
+import cats.data.ReaderT
+import net.kgtkr.twitter_tools.domain.models.Token
 
 trait UserCacheSYM[F[_]] {
-  def lookupUsers(ids: Set[UserId]): F[Map[UserId, Raw]]
+  type Result[A] = ReaderT[F, Token, A]
+
+  def lookupUsers(ids: Set[UserId]): Result[Map[UserId, Raw]]
 }
 
 object UserCacheSYM {
@@ -23,15 +29,17 @@ final class UserCacheImpl[F[_]: Monad: RawRepositoryCmdSYM: RawRepositoryQuerySY
     extends UserCacheSYM[F] {
   override def lookupUsers(
       ids: Set[UserId]
-  ): F[Map[UserId, Raw]] = {
+  ): Result[Map[UserId, Raw]] = {
     for {
-      dbUsers <- RawRepositoryQuerySYM[F]
-        .findLatest[UserRaw](ids.toList)
-        .map(_.map(x => (x.id, x)).toMap)
+      dbUsers <- ReaderT.liftF(
+        RawRepositoryQuerySYM[F]
+          .findLatest[UserRaw](ids.toList)
+          .map(_.map(x => (x.id, x)).toMap)
+      )
       fetchedUserArr <- TwitterClientQuerySYM[F].lookupUsers(
         ids.diff(dbUsers.keySet).toList
       )
-      _ <- RawRepositoryCmdSYM[F].insertAll(fetchedUserArr)
+      _ <- ReaderT.liftF(RawRepositoryCmdSYM[F].insertAll(fetchedUserArr))
     } yield (dbUsers.toSeq ++ fetchedUserArr.map(x => (x.id, x)).toSeq).toMap
   }
 }
